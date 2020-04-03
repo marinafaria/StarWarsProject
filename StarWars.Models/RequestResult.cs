6c7b91ec3be7b70c9ebcb8aa0b7df221de5a6156
Marina Faria <maarina.faria@gmail.com>
2020-04-03 16:26:09 -0300

    request

---
+++ StarWars.Models/RequestResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarWars.Models
+{
+    public class RequestResult
+    {
+        public int Count { get; set; }
+        public string Previous { get; set; }
+        public string Next { get; set; }
+        public List<People> Results { get; set; }
+    }
+}
